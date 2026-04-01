c93e0dd1bbed98e4b50128e50ffd308e6affa821
saijeet7 <saijeet2019@gmail.com>
2026-04-01 21:47:48 +0545

    feat: fetch and render recipe view with loading spinner and polyfills

---
+++ dist/forkify.7f8fc509.js
@@ -0,0 +1,2 @@
+const recipeContainer=document.querySelector(".recipe"),timeout=function(e){return new Promise(function(o,t){setTimeout(function(){t(Error(`Request took too long! Timeout after ${e} second`))},1e3*e)})};
+//# sourceMappingURL=forkify.7f8fc509.js.map
